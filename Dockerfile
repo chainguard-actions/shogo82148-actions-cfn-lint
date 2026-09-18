60d6afcdb58142da85fbbc0fcdf579b395a6cd53
actionchain <actionchain@chainguard.dev>
2026-09-18 05:40:57 +0000

    Update shogo82148/actions-cfn-lint@v4.79.0

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM python:3.14.7-alpine3.24@sha256:c6ead215bfd31f1e433d968853b7a769989117115b728874824e6c0a27cb96fc
+
+# install cfn-lint
+COPY requirements.txt /requirements.txt
+RUN apk --no-cache add git bash && pip install --upgrade pip && pip install --no-cache -r /requirements.txt
+
+# install reviewdog
+ENV REVIEWDOG_VERSION=v0.21.1
+RUN wget -O - -q https://raw.githubusercontent.com/reviewdog/reviewdog/master/install.sh | sh -s -- -b /usr/local/bin/ "$REVIEWDOG_VERSION"
+
+COPY entrypoint.sh /entrypoint.sh
+
+ENTRYPOINT ["/entrypoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+export REVIEWDOG_GITHUB_API_TOKEN=$INPUT_GITHUB_TOKEN
+
+shopt -s globstar nullglob
+
+# enable debug if INPUT_RUNNER_DEBUG is set
+if [[ ${RUNNER_DEBUG:-} = '1' ]]; then
+    set -x
+fi
+
+cfn-lint --version
+
+cd "${GITHUB_WORKSPACE}/${INPUT_WORKING_DIRECTORY}" || exit 1
+
+# INPUT_CFN_LINT_ARGS is intentionally not quoted:
+# shellcheck disable=SC2086
+cfn-lint --format parseable --non-zero-exit-code none $INPUT_CFN_LINT_ARGS > /tmp/out.log || exit 1 
+
+# show out.log if debug is enabled
+if [[ ${RUNNER_DEBUG:-} = '1' ]]; then
+    cat /tmp/out.log >&2
+fi
+
+git config --global --add safe.directory "${PWD}" || exit 1
+
+# INPUT_REVIEWDOG_FLAGS is intentionally not quoted:
+# shellcheck disable=SC2086
+reviewdog \
+    -efm='%f:%l:%c:%e:%k:%t%n:%m' \
+    -name="${INPUT_TOOL_NAME}" \
+    -reporter="${INPUT_REPORTER}" \
+    -filter-mode="${INPUT_FILTER_MODE}" \
+    -fail-level="${INPUT_FAIL_LEVEL}" \
+    -level="${INPUT_LEVEL}" \
+    ${INPUT_REVIEWDOG_FLAGS} < /tmp/out.log
+exit_code=$?
+
+exit $exit_code
